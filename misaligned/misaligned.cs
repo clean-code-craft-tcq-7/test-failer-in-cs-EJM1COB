d538fb09fe6ecb62d30308a2d927c0a86ab8ec89
Ranjith <ejm1cob@bosch.com>
2025-08-05 12:06:11 +0530

    error change

--- misaligned/misaligned.cs
+++ misaligned/misaligned.cs
@@ -8,9 +8,9 @@ namespace MisalignedSpace {
             string[] minorColors = {"Blue", "Orange", "Green", "Brown", "Slate"};
             int Colour Number =0;
             Console.WriteLine("{0,-3} | {1,-7} | {2,-6}", "Serial No", "Major", "Minor");
-            int i = 0, j = 0;
-            for(i = 0; i < majorColors.Length; i++) {
-                for(j = 0; j < minorColors.Length; j++) {
+            
+            for(int i = 0; i < majorColors.Length; i++) {
+                for(int j = 0; j < minorColors.Length; j++) {
                     Console.WriteLine("{0,-3} | {1,-7} | {2,-6}",Colour Number, majorColors[i], minorColors[j]);
                     Colour Number++;
                 }
