d2af8e1d1226e8ddb0efd9f9fac371ff4b7d4729
Ranjith <ejm1cob@bosch.com>
2025-08-05 11:56:39 +0530

    syntax error correction

--- misaligned/misaligned.cs
+++ misaligned/misaligned.cs
@@ -6,11 +6,11 @@ namespace MisalignedSpace {
         static int printColorMap() {
             string[] majorColors = {"White", "Red", "Black", "Yellow", "Violet"};
             string[] minorColors = {"Blue", "Orange", "Green", "Brown", "Slate"};
-            int serial Number =0
+            int serial Number =0;
             int i = 0, j = 0;
             for(i = 0; i < majorColors.Length; i++) {
                 for(j = 0; j < minorColors.Length; j++) {
-                    Console.WriteLine("{0-3} | {1-7} | {2-6}",serial Number, majorColors[i], minorColors[j]);
+                    Console.WriteLine("{0,-3} | {1,-7} | {2,-6}",serial Number, majorColors[i], minorColors[j]);
                     serial Number++;
                 }
             }
